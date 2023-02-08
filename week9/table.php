8e0b2a105a3eea1228e2553133939d560d5d6cec
Jared De Leon <jbdeleon@student.apc.edu.ph>
2023-02-08 13:30:41 +0800

    table.php

---
+++ week9/table.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "webprogmi212";
+$password = "webprogmi212";
+$dbname = "jbdeleon_myguest";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE jbdeleon_myguest (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+website VARCHAR(100),
+comment TEXT,
+gender VARCHAR(30),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table jbdeleon_myguest created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
